97e3fd70374a89a39cb95833398bea303fc380eb
unknown <emad.m.alnagar@gmail.com>
2022-06-23 04:55:10 +0200

    starting routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <NavBar />
+        </header>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { AiOutlineSearch, AiOutlineShoppingCart, AiOutlineHeart } from "react-icons/ai";
 import { Nav, Brand, ListContainer, ListItem } from '../styles/navbar'
 
@@ -6,10 +7,10 @@ function NavBar() {
   return (
     <Nav>
       <Brand>
-        ecommerce
+        <Link to='/'>ecommerce</Link>
       </Brand>
       <ListContainer>
-        <ListItem>home</ListItem>
+        <ListItem><Link to='/'>home</Link></ListItem>
         <ListItem>products</ListItem>
         <ListItem>collections</ListItem>
       </ListContainer>
