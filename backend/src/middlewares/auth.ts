db06329f352f0b640f7c4e6db29e94f0f02747fa
unknown <emad.m.alnagar@gmail.com>
2023-07-19 15:48:00 +0200

    User middlewares

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,44 @@
+import JWT from "jsonwebtoken";
+import User from "../models/user";
+
+const JWT_SECRET = process.env.JWT_SECRET || 'secretcode';
+
+//Protected Routes token base
+export const isSignIn = async (
+  req: { headers: { authorization: string; }; user: string | JWT.JwtPayload; }, 
+  _res: any, 
+  next: () => void) => {
+  try {
+    const decode = JWT.verify(
+      req.headers.authorization,
+      JWT_SECRET
+    );
+    req.user = decode;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//admin acceess
+export const isAdmin = async (req: { user: { _id: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: { success: boolean; message: string; error?: unknown; }): void; new(): any; }; }; }, next: () => void) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      if (user.isAdmin !== true) {
+        return res.status(401).send({
+          success: false,
+          message: "UnAuthorized Access",
+        });
+      } else {
+        next();
+      }
+    }
+  } catch (error) {
+    res.status(401).send({
+      success: false,
+      error,
+      message: "User is not admin",
+    });
+  }
+};
