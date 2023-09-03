728e542cfaa0c4fee00b13b7e63f67a4952a2669
unknown <emad.m.alnagar@gmail.com>
2023-09-03 20:32:43 +0200

    Fetching users data

--- frontend/src/pages/admin/Users.tsx
+++ frontend/src/pages/admin/Users.tsx
@@ -1,14 +1,27 @@
 import { useSelector } from "react-redux";
 import SideNav from "../../components/SideNav";
+import ErrorBox from "../../components/ErrorBox";
+import LoadingBox from "../../components/LoadingBox";
 import { Container } from "../../styles/main";
 import { Fragment } from "react";
+import { Helmet } from "react-helmet";
 
 const AllUsers = () => {
-  const { users, error, loading } = useSelector((state: any) => state.users);
+  const { users, error, loading } = useSelector((state: any) => state.user);
   return (
     <Fragment>
+      <Helmet>
+        <title>Electronics-login</title>
+      </Helmet>
       <SideNav />
       <Container>
+      {
+        error ? <ErrorBox /> :
+        loading ? <LoadingBox /> :
+        users.map((user: { firstName: string }) => (
+          <div>{ user.firstName }</div>
+        ))
+      }
       </Container>
     </Fragment>
   )
