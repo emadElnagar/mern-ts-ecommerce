8d4e4fb4def476e039aa74a9b2346cdb62514c16
unknown <emad.m.alnagar@gmail.com>
2023-09-15 21:48:27 +0200

    Get all users

--- frontend/src/pages/admin/Users.tsx
+++ frontend/src/pages/admin/Users.tsx
@@ -1,25 +1,30 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SideNav from "../../components/SideNav";
 import ErrorBox from "../../components/ErrorBox";
 import LoadingBox from "../../components/LoadingBox";
 import { Container } from "../../styles/main";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { Helmet } from "react-helmet";
+import { GetAllUsers } from "../../features/UserFeatures";
 
 const AllUsers = () => {
+  const dispatch = useDispatch();
   const { users, error, loading } = useSelector((state: any) => state.user);
+  useEffect(() => {
+    dispatch(GetAllUsers());
+  }, [dispatch]);
   return (
     <Fragment>
       <Helmet>
-        <title>Electronics-login</title>
+        <title>Electronics-Admin</title>
       </Helmet>
       <SideNav />
       <Container>
       {
         error ? <ErrorBox /> :
         loading ? <LoadingBox /> :
-        users.map((user: { firstName: string }) => (
-          <div>{ user.firstName }</div>
+        users.map((user: { _id: string, firstName: string }) => (
+          <div key={user._id}>{ user.firstName }</div>
         ))
       }
       </Container>
