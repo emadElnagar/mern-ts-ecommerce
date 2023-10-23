a19ebb485c36ccd3fc5b2dfe055845a7812ccf8f
unknown <emad.m.alnagar@gmail.com>
2023-10-23 17:42:54 +0200

    Category initialstate

---
+++ frontend/src/features/CategoryFeatures.ts
@@ -0,0 +1,20 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = 'http://localhost:5000/api/categories';
+
+export interface Category {
+  _id?: object;
+  title: string;
+  author: object;
+}
+
+interface CategoryState {
+  categories: Category[],
+  category: Category | null,
+}
+
+const initialState: CategoryState = {
+  categories: [],
+  category: null,
+}
