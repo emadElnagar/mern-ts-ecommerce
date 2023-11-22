99741c4b60f12f1a0ab040adff8fd22fd8026148
unknown <emad.m.alnagar@gmail.com>
2023-11-22 22:48:20 +0200

    Get all categories

--- frontend/src/components/CategoryList.tsx
+++ frontend/src/components/CategoryList.tsx
@@ -1,22 +1,22 @@
-import { Fragment } from "react";
 import { DeleteButton, Slide, UpdateButton } from "../styles/main";
 import { HiPencil } from 'react-icons/hi';
 import { MdDelete } from 'react-icons/md';
 
-const CategoryList = () => {
+type categoryProps = {
+  title: string
+}
+
+const CategoryList = (category: categoryProps) => {
   return (
-    <Fragment>
-      <h1 className="text-center">categories list</h1>
       <Slide>
         <div>
-          <h4>category title</h4>
+          <h4>{category.title}</h4>
         </div>
         <div>
           <UpdateButton><HiPencil /></UpdateButton>
           <DeleteButton><MdDelete /></DeleteButton>
         </div>
       </Slide>
-    </Fragment>
   )
 }
 
--- frontend/src/pages/admin/Categories.tsx
+++ frontend/src/pages/admin/Categories.tsx
@@ -1,25 +1,38 @@
 import { Fragment } from "react";
+import { useEffect } from "react";
 import CategoryForm from "../../components/CategoryForm";
-import { Container, Section } from "../../styles/main";
+import { Container, DeleteButton, Section, Slide, UpdateButton } from "../../styles/main";
 import { Helmet } from "react-helmet";
 import CategoryList from "../../components/CategoryList";
+import { useDispatch, useSelector } from "react-redux";
+import { GetAllCategories } from "../../features/CategoryFeatures";
 
 const CategoriesPage = () => {
-    return (
-      <Fragment>
-        <Helmet>
-          <title>Electronics-admin</title>
-        </Helmet>
-        <Container>
-          <Section>
-            <CategoryForm />
-          </Section>
-          <Section>
-            <CategoryList />
-          </Section>
-        </Container>
-      </Fragment>
-    )
+  const dispatch = useDispatch();
+  const { categories } = useSelector((state: any) => state.category);
+  useEffect(() => {
+    dispatch(GetAllCategories());
+  }, [dispatch]);
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Electronics-admin</title>
+      </Helmet>
+      <Container>
+        <Section>
+          <CategoryForm />
+        </Section>
+        <Section>
+          <h1 className="text-center">categories list</h1>
+          {
+            categories.map((category: { _id: React.Key, title: string }) => (
+              <CategoryList key={ category._id } title={ category.title } />
+            ))
+          }
+        </Section>
+      </Container>
+    </Fragment>
+  )
   }
   
   export default CategoriesPage;
