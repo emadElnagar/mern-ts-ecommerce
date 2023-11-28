afa13ab0a3847e8235c94a48d6b3898140d9f3e4
unknown <emad.m.alnagar@gmail.com>
2023-11-28 21:15:38 +0200

    Update category implementation

--- frontend/src/components/CategoryList.tsx
+++ frontend/src/components/CategoryList.tsx
@@ -1,9 +1,9 @@
 import { DeleteButton, Slide, UpdateButton } from "../styles/main";
 import { HiPencil } from 'react-icons/hi';
-import { MdDelete } from 'react-icons/md';
+import { MdDelete, MdOutlineTimeToLeave } from 'react-icons/md';
 import { Key } from "react";
 import { useDispatch } from "react-redux";
-import { DeleteCategory, GetAllCategories } from "../features/CategoryFeatures";
+import { DeleteCategory, GetAllCategories, UpdateCategory } from "../features/CategoryFeatures";
 import swal from 'sweetalert2';
 
 type categoryProps = {
@@ -11,8 +11,13 @@ type categoryProps = {
   title: string
 }
 
+type UpdateCategoryType = {
+  title: string
+}
+
 const CategoryList = (category: categoryProps) => {
   const dispatch = useDispatch();
+  let titleInput: HTMLInputElement;
   // Delete category
   const handleDelete = (id: Key) => {
     swal.fire({
@@ -35,13 +40,48 @@ const CategoryList = (category: categoryProps) => {
       }
     });
   }
+  // Update category
+  const handleUpdate = (_id: Key) => {
+    swal.fire<UpdateCategoryType>({
+      title: 'Update category',
+      html: `
+        <input type="text" id="title" class="swal2-input" value="${category.title}">
+      `,
+      showCancelButton: true,
+      confirmButtonText: 'Confirm',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      focusConfirm: false,
+      didOpen: () => {
+        const popup = swal.getPopup()!
+        titleInput = popup.querySelector('#title') as HTMLInputElement;
+        titleInput.onkeyup = (event) => event.key === 'Enter' && swal.clickConfirm();
+      },
+      preConfirm: () => {
+        const title = titleInput.value;
+        if (!MdOutlineTimeToLeave) {
+          swal.showValidationMessage(`Please enter title and password`)
+        }
+        return { title }
+      },
+    }).then((result) => {
+      const  title  = result.value?.title;
+      if (result.isConfirmed) {
+        dispatch(UpdateCategory({
+          _id,
+          title
+        }));
+        dispatch(GetAllCategories());
+      }
+    });
+  }
   return (
     <Slide>
       <div>
         <h4>{category.title}</h4>
       </div>
       <div>
-        <UpdateButton><HiPencil /></UpdateButton>
+        <UpdateButton onClick={() => handleUpdate(category._id)}><HiPencil /></UpdateButton>
         <DeleteButton onClick={() => handleDelete(category._id)}><MdDelete /></DeleteButton>
       </div>
     </Slide>
--- frontend/src/pages/admin/Categories.tsx
+++ frontend/src/pages/admin/Categories.tsx
@@ -45,4 +45,3 @@ const CategoriesPage = () => {
 }
 
 export default CategoriesPage;
-  
