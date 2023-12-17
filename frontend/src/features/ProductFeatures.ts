01927d0f709301f6a99afd6c22d8f2462354ab03
unknown <emad.m.alnagar@gmail.com>
2023-12-17 22:30:30 +0200

    Create new product service

--- frontend/src/features/ProductFeatures.ts
+++ frontend/src/features/ProductFeatures.ts
@@ -26,10 +26,45 @@ const initialState: ProductState = {
   error: null
 }
 
+// Create new product
+export const NewProduct: any = createAsyncThunk("products/new",async (product: any, { rejectWithValue }) => {
+  try {
+    const response = await axios.post(`${url}/new`, {
+      name: product.name,
+      description: product.description,
+      brand: product.brand,
+      price: product.price,
+      discount: product.discount,
+      countInStock: product.countInStock,
+      images: product.images,
+      category: product.category,
+      seller: product.seller
+    });
+    return response.data;
+  } catch (error: any) {
+    return rejectWithValue(error.message);
+  }
+});
+
 const productSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // Create new product
+      .addCase(NewProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(NewProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.products.push(action.payload);
+      })
+      .addCase(NewProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error;
+      })
+  }
 });
 
 export default productSlice.reducer;
