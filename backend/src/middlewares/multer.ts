445c7bdd2c3aa9acb206597ce56505b723a7da80
unknown <emad.m.alnagar@gmail.com>
2023-12-24 20:29:11 +0200

    multer middleware

---
+++ backend/src/middlewares/multer.ts
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, './uploads/images')
+  },
+  filename: function (_req, file, cb) {
+    cb(null, new Date().toDateString() + file.originalname)
+  }
+});
+
+export const upload = multer({ storage: storage });
