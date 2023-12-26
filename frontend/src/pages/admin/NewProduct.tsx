2e3357153bf455a2f48c07cbb09f65865c12ff1c
unknown <emad.m.alnagar@gmail.com>
2023-12-26 20:54:06 +0200

    new product form data

--- frontend/src/pages/admin/NewProduct.tsx
+++ frontend/src/pages/admin/NewProduct.tsx
@@ -1,6 +1,6 @@
-import { Fragment, Key } from "react";
+import { ChangeEvent, Fragment, Key, SetStateAction, useState } from "react";
 import { Helmet } from "react-helmet";
-import { HeaderCenter, Main, Section } from "../../styles/main";
+import { Button, HeaderCenter, Main, Section } from "../../styles/main";
 import SideNav from "../../components/SideNav";
 import { Content } from "../../styles/admin";
 import { Field, Input, Select, Textarea } from "../../styles/form";
@@ -8,6 +8,33 @@ import { useSelector } from "react-redux";
 
 const NewProduct = () => {
   const { categories } = useSelector((state: any) => state.category);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [brand, setBrand] = useState('');
+  const [price, setPrice] = useState<number|null>(null);
+  const [discount, setDiscount] = useState<number|null>(null);
+  const [countInStock, setCountInStock] = useState<number|null>(null);
+  const [category, setCategory] = useState('');
+  const [images, setImages] = useState<string[]>([]);
+  // Change price
+  const onPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = !Number.isNaN(e.target.valueAsNumber) ? e.target.valueAsNumber : null;
+    setPrice(value);
+  }
+  // Change discount
+  const onDiscountChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = !Number.isNaN(e.target.valueAsNumber) ? e.target.valueAsNumber : null;
+    setDiscount(value);
+  }
+  // Change count in stock
+  const onCountInStockChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = !Number.isNaN(e.target.valueAsNumber) ? e.target.valueAsNumber : null;
+    setCountInStock(value);
+  }
+  // handle form submit
+  const handleSbmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
   return (
     <Fragment>
       <Helmet>
@@ -18,33 +45,33 @@ const NewProduct = () => {
         <Content>
           <Section>
             <HeaderCenter>add new product</HeaderCenter>
-            <form className="product-form">
+            <form method="POST" onSubmit={handleSbmit} className="product-form" encType="multipart/form-data" acceptCharset="*/images">
               <Field>
-                <Input type="text" id="title" />
-                <label htmlFor="title">title</label>
+                <Input onChange={(e: { target: { value: SetStateAction<string>; }; }) => setName(e.target.value)} type="text" id="name" />
+                <label htmlFor="name">name</label>
               </Field>
               <Field>
-                <Textarea id="description"></Textarea>
+                <Textarea onChange={(e: { target: { value: SetStateAction<string>; }; }) => setDescription(e.target.value)} id="description"></Textarea>
                 <label htmlFor="description">description</label>
               </Field>
               <Field>
-                <Input type="text" id="brand" />
+                <Input onChange={(e: { target: { value: SetStateAction<string>; }; }) => setBrand(e.target.value)} type="text" id="brand" />
                 <label htmlFor="brand">brand</label>
               </Field>
               <Field>
-                <Input type="number" id="price" />
+                <Input onChange={onPriceChange} type="number" min="0" id="price" />
                 <label htmlFor="price">price</label>
               </Field>
               <Field>
-                <Input type="number" id="discount" />
+                <Input onChange={onDiscountChange} type="number" min="0" id="discount" />
                 <label htmlFor="discount">discount</label>
               </Field>
               <Field>
-                <Input type="number" id="countinstock" />
+                <Input onChange={onCountInStockChange} type="number" min="0" id="countinstock" />
                 <label htmlFor="countinstock">count in stock</label>
               </Field>
               <Field>
-                <Select name="category" id="category">
+                <Select onChange={(e: { target: { value: SetStateAction<string>; }; }) => setCategory(e.target.value)} name="category" id="category">
                   {
                     categories.map((category: { _id: Key; title: string }) => (
                       <option key={ category._id } value={`${ category.title }`}>{ category.title }</option>
@@ -54,9 +81,10 @@ const NewProduct = () => {
                 <label htmlFor="category">category</label>
               </Field>
               <Field>
-                <Input type="file" id="images" accept="image/*" />
+                <Input type="file" id="images" accept="image/*" multiple/>
                 <label htmlFor="images">images</label>
               </Field>
+              <Button type="submit">submit</Button>
             </form>
           </Section>
         </Content>
