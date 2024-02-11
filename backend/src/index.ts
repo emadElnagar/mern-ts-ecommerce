ba8173cfb7b73ce7e3e3b72c0d559d6985e9c9fb
unknown <emad.m.alnagar@gmail.com>
2024-02-12 00:27:00 +0200

    Working with dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,14 @@ import mongoose from 'mongoose';
 import userRouter from './routes/userRoutes';
 import productRouter from './routes/productRoutes';
 import categoryRouter from './routes/categoryRoutes';
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/electronics', (err) => {
   if (err) {
