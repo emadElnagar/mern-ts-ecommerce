fafcd60f957373ea486a97742b933d68656d4e6a
unknown <emad.m.alnagar@gmail.com>
2024-03-18 23:19:20 +0200

    Generate JWT token

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -42,3 +42,22 @@ export const isAdmin = async (req: { user: { _id: any; }; }, res: { status: (arg
     });
   }
 };
+
+// Generate Token
+export const generateToken = (user: {
+  _id: object;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string; 
+}) => {
+  return JWT.sign({
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    role: user.role
+  }, process.env.JWT_SECRET!, {
+    expiresIn: '3d',
+  });
+};
