4d99330550b4a01a00983012f55fa6530bbce8bf
unknown <emad.m.alnagar@gmail.com>
2024-06-19 19:49:45 +0200

    Number of product images

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -1,22 +1,28 @@
 import { Router } from "express";
-import { deleteProduct, getAllProducts, getSingleProduct, newProduct, updateProduct } from '../controllers/productController';
+import {
+  deleteProduct,
+  getAllProducts,
+  getSingleProduct,
+  newProduct,
+  updateProduct,
+} from "../controllers/productController";
 import { upload } from "../middlewares/multer";
 
 const productRouter = Router();
 
 // GET ALL PRODUCTS
-productRouter.get('/all', getAllProducts);
+productRouter.get("/all", getAllProducts);
 
 // GET SINGLE PRODUCT
-productRouter.get('/:slug', getSingleProduct);
+productRouter.get("/:slug", getSingleProduct);
 
 // CREATE NEW PRODUCT
-productRouter.post('/new', upload.array('product'),newProduct);
+productRouter.post("/new", upload.array("product", 6), newProduct);
 
 // UPDATE PRODUCT
-productRouter.put('/:id/update', updateProduct);
+productRouter.put("/:id/update", updateProduct);
 
 // DELETE PRODUCT
-productRouter.delete('/:id/delete', deleteProduct);
+productRouter.delete("/:id/delete", deleteProduct);
 
 export default productRouter;
