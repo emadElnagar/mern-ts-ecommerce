6b2ca0fcec7ad899764a645a82f89f573d5e3857
unknown <emad.m.alnagar@gmail.com>
2025-02-05 22:53:47 +0200

    Start Admin dashboard main page

--- frontend/src/pages/admin/index.tsx
+++ frontend/src/pages/admin/index.tsx
@@ -1,15 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import SideNav from "../../components/SideNav";
-import { Container } from "../../styles/main";
+import { Container, Grid, Section } from "../../styles/main";
+import { useDispatch, useSelector } from "react-redux";
+import { GetAllProducts } from "../../features/ProductFeatures";
+import { Helmet } from "react-helmet";
 
 const AdminMainPage = () => {
+  const dispatch = useDispatch();
+  const { products, error, loading } = useSelector(
+    (state: any) => state.product
+  );
+  useEffect(() => {
+    dispatch(GetAllProducts());
+  }, [dispatch]);
   return (
     <Fragment>
+      <Helmet>
+        <title>Electronics-Admin</title>
+      </Helmet>
       <SideNav />
       <Container>
+        <Section>
+          <Grid>
+            <h1>Admin Main Page</h1>
+          </Grid>
+        </Section>
       </Container>
     </Fragment>
-  )
-}
+  );
+};
 
 export default AdminMainPage;
