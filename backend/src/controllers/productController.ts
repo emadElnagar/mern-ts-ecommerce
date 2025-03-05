044903d622ec1a9bb5721030a5216f8ca971f0ea
unknown <emad.m.alnagar@gmail.com>
2025-03-05 23:42:57 +0200

    Fix slug error

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -110,7 +110,7 @@ export const updateProduct: RequestHandler = async (req, res) => {
     rating: req.body.rating,
     category: req.body.category,
   };
-  Product.updateOne({ _id: req.params.slug }, { $set: newProduct })
+  Product.updateOne({ slug: req.params.slug }, { $set: newProduct })
     .then((_result) => {
       res.status(200).json({
         message: "Product updated successfully",
