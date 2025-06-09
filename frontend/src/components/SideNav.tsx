5b4c638032e83a897417e1d7db7d40173cc25845
unknown <emad.m.alnagar@gmail.com>
2025-06-09 21:21:06 +0200

    Adding Go website button

--- frontend/src/components/SideNav.tsx
+++ frontend/src/components/SideNav.tsx
@@ -6,6 +6,9 @@ import { IoAdd } from "react-icons/io5";
 const SideNav = () => {
   return (
     <Nav>
+      <Link to="/">
+        <NavButton className="home-button">Go website</NavButton>
+      </Link>
       <Link to="/admin">
         <NavButton>dashboard</NavButton>
       </Link>
@@ -16,10 +19,12 @@ const SideNav = () => {
         <NavButton>categories</NavButton>
       </Link>
       <Link to="/admin/products/new">
-        <NavButton><IoAdd /> new product</NavButton>
+        <NavButton>
+          <IoAdd /> new product
+        </NavButton>
       </Link>
     </Nav>
-  )
-}
+  );
+};
 
 export default SideNav;
