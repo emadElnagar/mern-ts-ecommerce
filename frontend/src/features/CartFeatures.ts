b512dcdef19ac5d6a1b2b2bc79fa2cf3fcf0e689
unknown <emad.m.alnagar@gmail.com>
2025-07-08 22:11:53 +0200

    Add to cart feature

--- frontend/src/features/CartFeatures.ts
+++ frontend/src/features/CartFeatures.ts
@@ -1,12 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export interface cart {
+interface product {
   _id: object;
   quantity: number;
 }
 
 interface CartState {
-  cart: cart[];
+  cart: product[];
   isLoading: boolean;
   error: object | null;
 }
@@ -16,11 +16,48 @@ const initialState: CartState = {
   error: null,
 };
 
+// Add to cart
+export const addToCart: any = createAsyncThunk(
+  "cart/add",
+  async (product: product, { rejectWithValue }) => {
+    try {
+      const data = {
+        id: product._id,
+        quantity: product.quantity,
+      };
+      localStorage.setItem("cart", JSON.stringify(data));
+      return data;
+    } catch (error: any) {
+      const message =
+        error.response?.data?.message ||
+        error.message ||
+        "Something went wrong";
+      return rejectWithValue(message);
+    }
+  }
+);
+
 const cartSlice = createSlice({
   name: "category",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      // Add to cart
+      .addCase(addToCart.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(addToCart.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.cart.push(action.payload);
+      })
+      .addCase(addToCart.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export default cartSlice.reducer;
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/UserFeatures';
 import categoryReducer from './features/CategoryFeatures';
 import productReducer from './features/ProductFeatures';
+import cartReducer from './features/CartFeatures';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     category: categoryReducer,
     product: productReducer,
+    cart: cartReducer,
   }
 });
 
