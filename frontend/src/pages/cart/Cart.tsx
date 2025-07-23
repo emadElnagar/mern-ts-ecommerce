86a76f5cbec02002524ea6bb32b2f00357b4b914
unknown <emad.m.alnagar@gmail.com>
2025-07-23 19:33:50 +0200

    Display cart products

--- frontend/src/pages/cart/Cart.tsx
+++ frontend/src/pages/cart/Cart.tsx
@@ -14,6 +14,8 @@ import {
 } from "../../styles/cart";
 import { useDispatch, useSelector } from "react-redux";
 import { getCart } from "../../features/CartFeatures";
+import LoadingBox from "../../components/LoadingBox";
+import ErrorBox from "../../components/ErrorBox";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -21,80 +23,77 @@ const Cart = () => {
     dispatch(getCart());
   }, [dispatch]);
   const { cart, error, isLoading } = useSelector((state: any) => state.cart);
+  console.log(cart);
   return (
     <Fragment>
       <Helmet>
         <title>Electronics-cart</title>
       </Helmet>
-      <Section>
-        <Container>
-          <TableWrapper>
-            <StyledTable>
-              <TableHead>
-                <TableRow>
-                  <TableHeader>Product</TableHeader>
-                  <TableHeader>Price</TableHeader>
-                  <TableHeader>Quantity</TableHeader>
-                  <TableHeader>Total</TableHeader>
-                  <TableHeader>Actions</TableHeader>
-                </TableRow>
-              </TableHead>
-              <tbody>
-                <TableRow>
-                  <TableData>
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      <ProductImage
-                        src="https://placehold.co/50x50"
-                        alt="Product"
-                      />
-                      Wireless Mouse
-                    </div>
-                  </TableData>
-                  <TableData>$25.99</TableData>
-                  <TableData>
-                    <QuantityInput type="number" value="2" readOnly />
-                  </TableData>
-                  <TableData>$51.98</TableData>
-                  <TableData>
-                    <RemoveButton>Remove</RemoveButton>
-                  </TableData>
-                </TableRow>
-
-                <TableRow>
-                  <TableData>
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      <ProductImage
-                        src="https://placehold.co/50x50"
-                        alt="Product"
-                      />
-                      Bluetooth Headphones
-                    </div>
-                  </TableData>
-                  <TableData>$89.99</TableData>
-                  <TableData>
-                    <QuantityInput type="number" value="1" readOnly />
-                  </TableData>
-                  <TableData>$89.99</TableData>
-                  <TableData>
-                    <RemoveButton>Remove</RemoveButton>
-                  </TableData>
-                </TableRow>
-                <TableRow>
-                  <TableData colSpan={3}>
-                    <strong>Total:</strong>
-                  </TableData>
-                  <TableData>
-                    <strong>$141.97</strong>
-                  </TableData>
-                  <TableData>
-                    <RemoveButton>Clear Cart</RemoveButton>
-                  </TableData>
-                </TableRow>
-              </tbody>
-            </StyledTable>
-          </TableWrapper>
-        </Container>
-      </Section>
+      <Container>
+        <Section>
+          {isLoading ? (
+            <LoadingBox />
+          ) : error ? (
+            <ErrorBox message={error} />
+          ) : cart.length === 0 ? (
+            <ErrorBox message="Your cart is empty" />
+          ) : (
+            <TableWrapper>
+              <StyledTable>
+                <TableHead>
+                  <TableRow>
+                    <TableHeader>Product</TableHeader>
+                    <TableHeader>Price</TableHeader>
+                    <TableHeader>Quantity</TableHeader>
+                    <TableHeader>Total</TableHeader>
+                    <TableHeader>Actions</TableHeader>
+                  </TableRow>
+                </TableHead>
+                <tbody>
+                  {cart.map((product: any) => (
+                    <TableRow key={product._id}>
+                      <TableData>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          <ProductImage
+                            src={`http://localhost:5000/${product.images[0]}`}
+                            alt="There is a problem showing your photos"
+                          />
+                          {product.name}
+                        </div>
+                      </TableData>
+                      <TableData>${product.Price - product.discount}</TableData>
+                      <TableData>
+                        <QuantityInput
+                          type="number"
+                          value={product.quantity}
+                          readOnly
+                        />
+                      </TableData>
+                      <TableData>
+                        ${(product.price - product.discount) * product.quantity}
+                      </TableData>
+                      <TableData>
+                        <RemoveButton>Remove</RemoveButton>
+                      </TableData>
+                    </TableRow>
+                  ))}
+                  <TableRow>
+                    <TableData colSpan={3}>
+                      <strong>Total:</strong>
+                    </TableData>
+                    <TableData>
+                      <strong>$141.97</strong>
+                    </TableData>
+                    <TableData>
+                      <RemoveButton>Clear Cart</RemoveButton>
+                    </TableData>
+                  </TableRow>
+                </tbody>
+              </StyledTable>
+            </TableWrapper>
+          )}
+        </Section>
+      </Container>
     </Fragment>
   );
 };
