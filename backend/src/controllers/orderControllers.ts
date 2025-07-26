9ee1c76015fd28aad5c524c4c95a326680a6d59f
unknown <emad.m.alnagar@gmail.com>
2025-07-26 21:54:27 +0200

    Get all orders - API

--- backend/src/controllers/orderControllers.ts
+++ backend/src/controllers/orderControllers.ts
@@ -32,3 +32,18 @@ export const createOrder = async (req: AuthenticatedRequest, res: Response) => {
     });
   }
 };
+
+// Get all orders
+export const getAllOrders = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- backend/src/routes/orderRoutes.ts
+++ backend/src/routes/orderRoutes.ts
@@ -1,12 +1,19 @@
 import { RequestHandler, Router } from "express";
-import { isAuth } from "../middlewares/auth";
+import { isAdmin, isAuth } from "../middlewares/auth";
 import { AuthenticatedRequest } from "../types/authTypes";
-import { createOrder } from "../controllers/orderControllers";
+import { createOrder, getAllOrders } from "../controllers/orderControllers";
 
 const orderRouter = Router();
 
 orderRouter.post("/create", isAuth as RequestHandler, (req, res) =>
   createOrder(req as AuthenticatedRequest, res)
+);
+
+orderRouter.get(
+  "/",
+  isAuth as RequestHandler,
+  isAdmin as RequestHandler,
+  async (req, res) => getAllOrders(req as AuthenticatedRequest, res)
 );
 
 export default orderRouter;
