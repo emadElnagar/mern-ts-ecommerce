96e9aaf728cff976cbee8bf365be4e082349de2f
unknown <emad.m.alnagar@gmail.com>
2025-10-02 19:17:32 +0200

    Updating api

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -1,10 +1,9 @@
 /// <reference types="vite/client" />
 
 export const API_URL = import.meta.env.VITE_API_URL;
-export const USERS_API_URL = import.meta.env.VITE_URL + "/users";
-export const CATEGORY_API_URL = import.meta.env.VITE_API_URL + "/categories";
-export const PRODUCTS_API_URL = import.meta.env.VITE_API_URL + "/products";
-export const CART_API_URL = import.meta.env.VITE_API_URL + "/products/cart";
-export const WISHLIST_API_URL =
-  import.meta.env.VITE_API_URL + "/products/wishlist";
-export const ORDERS_API_URL = import.meta.env.VITE_API_URL + "/orders";
+export const USERS_API_URL = API_URL + "/api/users";
+export const CATEGORY_API_URL = API_URL + "/api/categories";
+export const PRODUCTS_API_URL = API_URL + "/api/products";
+export const CART_API_URL = API_URL + "/api/products/cart";
+export const WISHLIST_API_URL = API_URL + "/api/products/wishlist";
+export const ORDERS_API_URL = API_URL + "/api/orders";
