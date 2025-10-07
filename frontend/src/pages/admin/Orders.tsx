723450ab2cc0d5eb21371226a5b267ef0142425c
unknown <emad.m.alnagar@gmail.com>
2025-10-07 21:48:11 +0200

    Get orders loading and errors

--- frontend/src/pages/admin/Orders.tsx
+++ frontend/src/pages/admin/Orders.tsx
@@ -17,6 +17,8 @@ import { HiPencil } from "react-icons/hi";
 import { success, warning } from "../../styles/variables";
 import { useDispatch, useSelector } from "react-redux";
 import { GetAllOrders } from "../../features/OrderFeatures";
+import LoadingBox from "../../components/LoadingBox";
+import ErrorBox from "../../components/ErrorBox";
 
 const OrdersPage = () => {
   const dispatch = useDispatch();
@@ -34,76 +36,83 @@ const OrdersPage = () => {
         <Content>
           <Section>
             <HeaderCenter>Manage all orders</HeaderCenter>
-            <TableWrapper>
-              <StyledTable>
-                <TableHead>
-                  <TableRow>
-                    <TableHeader>Order</TableHeader>
-                    <TableHeader>Date</TableHeader>
-                    <TableHeader>Customer</TableHeader>
-                    <TableHeader>Total</TableHeader>
-                    <TableHeader>Payment</TableHeader>
-                    <TableHeader>Delivery</TableHeader>
-                    <TableHeader>Items</TableHeader>
-                    <TableHeader>Fulfillment</TableHeader>
-                    <TableHeader>Actions</TableHeader>
-                  </TableRow>
-                </TableHead>
-                <tbody>
-                  {orders &&
-                    orders.length > 0 &&
-                    orders.map((order: any) => (
-                      <TableRow key={order._id}>
-                        <TableData>{order.orderNumber}</TableData>
-                        <TableData>
-                          {new Date(order.createdAt).toLocaleDateString()}
-                        </TableData>
-                        <TableData style={{ textTransform: "capitalize" }}>
-                          {order.customer.firstName} {order.customer.lastName}
-                        </TableData>
-                        <TableData>${order.totalPrice.toFixed(2)}</TableData>
-                        <TableData
-                          style={{ color: order.isPaid ? success : warning }}
-                        >
-                          {order.paymentResult && order.paymentResult.paidAt
-                            ? "Paid"
-                            : "pending"}
-                        </TableData>
-                        <TableData
-                          style={{
-                            color:
-                              order.shippingStatus &&
-                              order.shippingStatus === "Delivered"
-                                ? success
-                                : warning,
-                          }}
-                        >
-                          {order.shippingStatus}
-                        </TableData>
-                        <TableData>{order.orderItems.length}</TableData>
-                        <TableData
-                          style={{
-                            color:
-                              order.shippingStatus &&
-                              order.shippingStatus === "Delivered"
-                                ? success
-                                : warning,
-                          }}
-                        >
-                          {order.shippingStatus &&
-                          order.shippingStatus === "Delivered"
-                            ? "Fulfilled"
-                            : "Unfulfilled"}
-                        </TableData>
-                        <TableData>
-                          <FaRegEye title="View" /> | <HiPencil title="Edit" />{" "}
-                          | <MdDelete title="Delete" />
-                        </TableData>
-                      </TableRow>
-                    ))}
-                </tbody>
-              </StyledTable>
-            </TableWrapper>
+            {isLoading ? (
+              <LoadingBox />
+            ) : error ? (
+              <ErrorBox message={error} />
+            ) : (
+              <TableWrapper>
+                <StyledTable>
+                  <TableHead>
+                    <TableRow>
+                      <TableHeader>Order</TableHeader>
+                      <TableHeader>Date</TableHeader>
+                      <TableHeader>Customer</TableHeader>
+                      <TableHeader>Total</TableHeader>
+                      <TableHeader>Payment</TableHeader>
+                      <TableHeader>Delivery</TableHeader>
+                      <TableHeader>Items</TableHeader>
+                      <TableHeader>Fulfillment</TableHeader>
+                      <TableHeader>Actions</TableHeader>
+                    </TableRow>
+                  </TableHead>
+                  <tbody>
+                    {orders &&
+                      orders.length > 0 &&
+                      orders.map((order: any) => (
+                        <TableRow key={order._id}>
+                          <TableData>{order.orderNumber}</TableData>
+                          <TableData>
+                            {new Date(order.createdAt).toLocaleDateString()}
+                          </TableData>
+                          <TableData style={{ textTransform: "capitalize" }}>
+                            {order.customer.firstName} {order.customer.lastName}
+                          </TableData>
+                          <TableData>${order.totalPrice.toFixed(2)}</TableData>
+                          <TableData
+                            style={{ color: order.isPaid ? success : warning }}
+                          >
+                            {order.paymentResult && order.paymentResult.paidAt
+                              ? "Paid"
+                              : "pending"}
+                          </TableData>
+                          <TableData
+                            style={{
+                              color:
+                                order.shippingStatus &&
+                                order.shippingStatus === "Delivered"
+                                  ? success
+                                  : warning,
+                            }}
+                          >
+                            {order.shippingStatus}
+                          </TableData>
+                          <TableData>{order.orderItems.length}</TableData>
+                          <TableData
+                            style={{
+                              color:
+                                order.shippingStatus &&
+                                order.shippingStatus === "Delivered"
+                                  ? success
+                                  : warning,
+                            }}
+                          >
+                            {order.shippingStatus &&
+                            order.shippingStatus === "Delivered"
+                              ? "Fulfilled"
+                              : "Unfulfilled"}
+                          </TableData>
+                          <TableData>
+                            <FaRegEye title="View" /> |{" "}
+                            <HiPencil title="Edit" /> |{" "}
+                            <MdDelete title="Delete" />
+                          </TableData>
+                        </TableRow>
+                      ))}
+                  </tbody>
+                </StyledTable>
+              </TableWrapper>
+            )}
           </Section>
         </Content>
       </Main>
