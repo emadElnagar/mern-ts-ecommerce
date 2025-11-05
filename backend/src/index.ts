70c6e8bfc0d4b58206de8897d8822589ac921649
unknown <emad.m.alnagar@gmail.com>
2025-11-05 22:59:23 +0200

    Limit request size

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 const mongoUri = process.env.MONGO_URI;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("uploads/images"));
 
